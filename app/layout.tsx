e074efafc1573ca868310928765da2bf810424b6
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2023-11-30 20:24:27 +0100

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
 
-const sora = Sora({ subsets: ["latin"] });
+const sora = Sora({
+    subsets: ["latin"],
+    weight: ["200", "400", "600", "700"],
+});
 
 export const metadata: Metadata = {
     title: "Create Next App",
