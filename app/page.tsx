faea7177a1a4fbfa601caaef3d4c912ea9e70e41
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2023-11-30 19:58:20 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
-        <main className="flex flex-col gap-8 justify-center items-center py-2 min-h-screen text-center">
-            <h1 className="text-lg font-bold lg:text-4xl">
-                This site is under construction
-            </h1>
-
-            <Image
-                src="/image.svg"
-                alt="Under Construction"
-                width={500}
-                height={500}
-            />
+        <main>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
         </main>
     );
 }
