c0eeb54cdd9a96f3f0695947acbd22a2a4103b89
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2023-12-02 03:25:37 +0100

    fix: nav

--- app/(components)/Header.tsx
+++ app/(components)/Header.tsx
@@ -7,7 +7,7 @@ import Nav from "./(nav)/Nav";
 
 export default function Header() {
     return (
-        <header className="flex justify-between p-4 items-end text-end px-16">
+        <header className="flex justify-between items-end py-8 px-16 text-end">
             <Image src={icon} alt="logo" width={67} height={67} />
             <Nav />
         </header>
