35b99a67b9f0cf152c7e45061efc5841d02cd3f2
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2023-12-04 00:27:06 +0100

    feat: notfound && mobile support

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function NoTFound() {
+    return (
+        <main className="flex flex-col gap-8 justify-center items-center py-2 min-h-screen text-center">
+            <h1 className="text-lg font-bold lg:text-4xl">
+                This site is under construction
+            </h1>
+
+            <Image
+                src="/images/hero.svg"
+                alt="Under Construction"
+                width={500}
+                height={500}
+            />
+        </main>
+    );
+}
