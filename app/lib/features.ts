be4bd491a3581a072b46b228b97517676940f65c
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2024-01-05 18:15:41 +0100

    feat: features section

--- app/lib/features.ts
+++ app/lib/features.ts
@@ -4,25 +4,21 @@ export type Feature = {
     link: `/features/${string}` | "";
 };
 
-/**
- * NOTE: The link property is used to link to the feature page.
- *
- */
 export const features: Feature[] = [
     {
         title: "Specialized IT solutions",
         description:
             "Abdellatif Dev will empower and guide you to realize your full potential.",
-        link: "",
+        link: "/features/specialized-it-solutions",
     },
     {
         title: "High quality solution",
         description: "We Provide High Quality Solutions for Your Business",
-        link: "",
+        link: "/features/quality-assurance",
     },
     {
         title: "UX / UI",
         description: "We creating visually appealing and user-friendly UI",
-        link: "",
+        link: "/features/ux-ui",
     },
 ];
