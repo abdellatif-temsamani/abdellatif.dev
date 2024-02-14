f89428c9a2029687d019a7b30e59290582fabc86
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2024-02-14 02:39:45 +0100

    feat: about us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
-import {
-    Features,
-    Hero,
-    InnovaTech,
-    Testimonials,
-} from "@/components/home";
+import { Features, Hero, InnovaTech, Testimonials } from "@/components/home";
 
 export default function Home() {
     return (
         <main>
-            <article className="flex flex-col gap-28">
+            <div className="flex flex-col gap-28">
                 <Hero />
                 <Features />
                 <InnovaTech />
                 <Testimonials />
-            </article>
+            </div>
         </main>
     );
 }
