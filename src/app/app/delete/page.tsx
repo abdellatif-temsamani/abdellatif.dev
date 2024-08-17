5b98a292e5a4163c4154b0a6841e01767e6d840a
abdellatif-temsamani <contact@abdellatif.dev>
2024-08-17 16:22:42 +0100

    init

--- src/app/app/delete/page.tsx
+++ src/app/app/delete/page.tsx
@@ -1,18 +1,19 @@
-import Image from "next/image";
-
-export default function NoTFound() {
+import Button from "@/components/shared/button";
+export default function Detele() {
+    async function sendRequest() { }
     return (
         <main className="flex flex-col gap-8 justify-center items-center py-2 min-h-screen text-center">
             <h1 className="text-lg font-bold lg:text-4xl">
-                This site is under construction
+                request deleting your data
             </h1>
-
-            <Image
-                src="/images/hero.svg"
-                alt="Under Construction"
-                width={500}
-                height={500}
-            />
+            <form action={sendRequest}>
+                <input
+                    type="text"
+                    placeholder="email"
+                    className="p-4 border-2 placeholder:text-sky-600 border-sky-400"
+                />
+                <Button>Send</Button>
+            </form>
         </main>
     );
 }
--- src/app/app/policy/page.tsx
+++ src/app/app/policy/page.tsx
@@ -1,18 +1,122 @@
-import Image from "next/image";
-
-export default function NoTFound() {
+export default function Policy() {
     return (
-        <main className="flex flex-col gap-8 justify-center items-center py-2 min-h-screen text-center">
-            <h1 className="text-lg font-bold lg:text-4xl">
-                This site is under construction
-            </h1>
-
-            <Image
-                src="/images/hero.svg"
-                alt="Under Construction"
-                width={500}
-                height={500}
-            />
+        <main>
+            <div className="flex flex-col gap-8">
+                <h2>Privacy Policy</h2>
+                <h3>Introduction</h3>
+                <p>
+                    Our privacy policy will help you understand what information
+                    we collect at abdellatif dev, how QR Code Scanner uses it,
+                    and what choices you have. QR Code Scanner built the QR Code
+                    Scanner app as a free app. This SERVICE is provided by QR
+                    Code Scanner at no cost and is intended for use as is. If
+                    you choose to use our Service, then you agree to the
+                    collection and use of information in relation with this
+                    policy. The Personal Information that we collect are used
+                    for providing and improving the Service. We will not use or
+                    share your information with anyone except as described in
+                    this Privacy Policy. The terms used in this Privacy Policy
+                    have the same meanings as in our Terms and Conditions, which
+                    is accessible in our website, unless otherwise defined in
+                    this Privacy Policy.
+                </p>
+                <h3>Information Collection and Use</h3>
+                <p>
+                    For a better experience while using our Service, we may
+                    require you to provide us with certain personally
+                    identifiable information, including but not limited to users
+                    name, email address, gender, location, pictures. The
+                    information that we request will be retained by us and used
+                    as described in this privacy policy. The app does use third
+                    party services that may collect information used to identify
+                    you.
+                </p>
+                <h3>Cookies</h3>
+                <p>
+                    Cookies are files with small amount of data that is commonly
+                    used an anonymous unique identifier. These are sent to your
+                    browser from the website that you visit and are stored on
+                    your devices’s internal memory.
+                </p>
+                <p>
+                    This Services does not uses these “cookies” explicitly.
+                    However, the app may use third party code and libraries that
+                    use “cookies” to collection information and to improve their
+                    services. You have the option to either accept or refuse
+                    these cookies, and know when a cookie is being sent to your
+                    device. If you choose to refuse our cookies, you may not be
+                    able to use some portions of this Service.
+                </p>
+                <h3>Location Information</h3>
+                <p>
+                    Some of the services may use location information
+                    transmitted from users’ mobile phones. We only use this
+                    information within the scope necessary for the designated
+                    service.
+                </p>
+                <h3>Device Information</h3>
+                <p>
+                    We collect information from your device in some cases. The
+                    information will be utilized for the provision of better
+                    service and to prevent fraudulent acts. Additionally, such
+                    information will not include that which will identify the
+                    individual user.
+                </p>
+                <h3>Service Providers</h3>
+                <p>
+                    We may employ third-party companies and individuals due to
+                    the following reasons:
+                </p>
+                <ul>
+                    <li>To facilitate our Service;</li>
+                    <li>To provide the Service on our behalf;</li>
+                    <li>To perform Service-related services; or</li>
+                    <li>To assist us in analyzing how our Service is used.</li>
+                </ul>
+                <p>
+                    We want to inform users of this Service that these third
+                    parties have access to your Personal Information. The reason
+                    is to perform the tasks assigned to them on our behalf.
+                    However, they are obligated not to disclose or use the
+                    information for any other purpose.
+                </p>
+                <h3>Security</h3>
+                <p>
+                    We value your trust in providing us your Personal
+                    Information, thus we are striving to use commercially
+                    acceptable means of protecting it. But remember that no
+                    method of transmission over the internet, or method of
+                    electronic storage is 100% secure and reliable, and we
+                    cannot guarantee its absolute security.
+                </p>
+                <h3>Children’s Privacy</h3>
+                <p>
+                    This Services do not address anyone under the age of 13. We
+                    do not knowingly collect personal identifiable information
+                    from children under 13. In the case we discover that a child
+                    under 13 has provided us with personal information, we
+                    immediately delete this from our servers. If you are a
+                    parent or guardian and you are aware that your child has
+                    provided us with personal information, please contact us so
+                    that we will be able to do necessary actions.
+                </p>
+                <h3>Changes to This Privacy Policy</h3>
+                <p>
+                    We may update our Privacy Policy from time to time. Thus,
+                    you are advised to review this page periodically for any
+                    changes. We will notify you of any changes by posting the
+                    new Privacy Policy on this page. These changes are effective
+                    immediately, after they are posted on this page.
+                </p>
+                <h3>Contact Us</h3>
+                <p>
+                    If you have any questions or suggestions about our Privacy
+                    Policy, do not hesitate to contact us. Contact Information:
+                </p>
+                <ul>
+                    <li>Email: contact@abdellatif.dev</li>
+                </ul>
+            </div>
         </main>
     );
 }
