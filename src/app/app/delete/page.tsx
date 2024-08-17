60e6e7d831c49e12ffa42c4c9bc32e34bd9fae29
abdellatif-temsamani <contact@abdellatif.dev>
2024-08-17 16:37:21 +0100

    init

--- src/app/app/delete/page.tsx
+++ src/app/app/delete/page.tsx
@@ -1,18 +1,28 @@
 import Button from "@/components/shared/button";
+import { getDeleteDataBody, sendEmail } from "@/lib/emailer";
+
 export default function Detele() {
-    async function sendRequest() { }
+    async function sendRequest(formData: FormData) {
+        "use server";
+        const email = formData.get("email") as string;
+        const body = getDeleteDataBody(email);
+        await sendEmail(body);
+    }
+
     return (
         <main className="flex flex-col gap-8 justify-center items-center py-2 min-h-screen text-center">
-            <h1 className="text-lg font-bold lg:text-4xl">
-                request deleting your data
-            </h1>
-            <form action={sendRequest}>
+            <form className="flex flex-col gap-8" action={sendRequest}>
+                <h1 className="text-lg font-bold lg:text-4xl">
+                    request deleting your data
+                </h1>
                 <input
+                    name="email"
+                    id="email"
                     type="text"
                     placeholder="email"
                     className="p-4 border-2 placeholder:text-sky-600 border-sky-400"
                 />
-                <Button>Send</Button>
+                <Button type="submit">Send</Button>
             </form>
         </main>
     );
---
+++ src/lib/emailer.ts
@@ -0,0 +1,47 @@
+import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+export type body = {
+    from: "web@abdellatif.dev";
+    to: "contact@abdellatif.dev";
+    subject: string;
+    text: string;
+    html: string;
+};
+
+function getEmailInstance(): nodemailer.Transporter<SMTPTransport.SentMessageInfo> {
+    return nodemailer.createTransport({
+        host: "mail.abdellatif.dev",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_ADDRESS,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+}
+
+export function getDeleteDataBody(email: string): body {
+    const text = `Dear Abdellatif dev
+        I request delete all my data with the following email
+         ${email}`;
+
+    const html = `<h1>Dear Abdellatif dev</h1>
+    <p>
+        I request delete all my data with the following email
+        <b>${email}</b> <br />
+    </p>`;
+
+    return {
+        from: "web@abdellatif.dev",
+        to: "contact@abdellatif.dev",
+        subject: "Vidange 360: inscription complète",
+        text: text,
+        html: html,
+    };
+}
+
+export async function sendEmail(body: body) {
+    const emailer = getEmailInstance();
+    await emailer.sendMail(body);
+}
