6b1acf8c9e1b03916091ed756405b6fd3269b8f3
abdellatif-temsamani <contact@abdellatif.dev>
2024-09-22 18:47:42 +0100

    init

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -2,9 +2,10 @@ export default function Policy() {
     return (
         <main>
             <div className="flex flex-col gap-8">
-                <h2>Privacy Policy</h2>
-                <h3>Introduction</h3>
-                <p>
+                <h1>Privacy Policy</h1>
+                <h2>Introduction</h2>
+
+                <p className="paragraph">
                     Our privacy policy will help you understand what information
                     we collect at abdellatif dev, how abdellatif dev uses it,
                     and what choices you have. abdellatif dev built the QR Code
@@ -20,8 +21,8 @@ export default function Policy() {
                     is accessible in our website, unless otherwise defined in
                     this Privacy Policy.
                 </p>
-                <h3>Information Collection and Use</h3>
-                <p>
+                <h2>Information Collection and Use</h2>
+                <p className="paragraph">
                     For a better experience while using our Service, we may
                     require you to provide us with certain personally
                     identifiable information, including but not limited to users
@@ -31,57 +32,71 @@ export default function Policy() {
                     party services that may collect information used to identify
                     you.
                 </p>
-                <h3>Cookies</h3>
-                <p>
-                    Cookies are files with small amount of data that is commonly
-                    used an anonymous unique identifier. These are sent to your
-                    browser from the website that you visit and are stored on
-                    your devices’s internal memory.
-                </p>
-                <p>
-                    This Services does not uses these “cookies” explicitly.
-                    However, the app may use third party code and libraries that
-                    use “cookies” to collection information and to improve their
-                    services. You have the option to either accept or refuse
-                    these cookies, and know when a cookie is being sent to your
-                    device. If you choose to refuse our cookies, you may not be
-                    able to use some portions of this Service.
-                </p>
-                <h3>Location Information</h3>
-                <p>
+                <h2>Cookies</h2>
+                <div className="paragraph">
+                    <p>
+                        Cookies are files with small amount of data that is
+                        commonly used an anonymous unique identifier. These are
+                        sent to your browser from the website that you visit and
+                        are stored on your devices’s internal memory.
+                    </p>
+                    <p>
+                        This Services does not uses these “cookies” explicitly.
+                        However, the app may use third party code and libraries
+                        that use “cookies” to collection information and to
+                        improve their services. You have the option to either
+                        accept or refuse these cookies, and know when a cookie
+                        is being sent to your device. If you choose to refuse
+                        our cookies, you may not be able to use some portions of
+                        this Service.
+                    </p>
+                </div>
+
+                <h2>Location Information</h2>
+
+                <p className="paragraph">
                     Some of the services may use location information
                     transmitted from users’ mobile phones. We only use this
                     information within the scope necessary for the designated
                     service.
                 </p>
-                <h3>Device Information</h3>
-                <p>
+                <h2>Device Information</h2>
+                <p className="paragraph">
                     We collect information from your device in some cases. The
                     information will be utilized for the provision of better
                     service and to prevent fraudulent acts. Additionally, such
                     information will not include that which will identify the
                     individual user.
                 </p>
-                <h3>Service Providers</h3>
-                <p>
-                    We may employ third-party companies and individuals due to
-                    the following reasons:
-                </p>
-                <ul>
-                    <li>To facilitate our Service;</li>
-                    <li>To provide the Service on our behalf;</li>
-                    <li>To perform Service-related services; or</li>
-                    <li>To assist us in analyzing how our Service is used.</li>
-                </ul>
-                <p>
-                    We want to inform users of this Service that these third
-                    parties have access to your Personal Information. The reason
-                    is to perform the tasks assigned to them on our behalf.
-                    However, they are obligated not to disclose or use the
-                    information for any other purpose.
-                </p>
-                <h3>Security</h3>
-                <p>
+                <h2>Service Providers</h2>
+                <div className="paragraph">
+                    <p>
+                        We may employ third-party companies and individuals due
+                        to the following reasons:
+                    </p>
+                    <ul>
+                        <li className="item">To facilitate our Service;</li>
+                        <li className="item">
+                            To provide the Service on our behalf;
+                        </li>
+                        <li className="item">
+                            To perform Service-related services; or
+                        </li>
+                        <li className="item">
+                            To assist us in analyzing how our Service is used.
+                        </li>
+                    </ul>
+                    <p>
+                        We want to inform users of this Service that these third
+                        parties have access to your Personal Information. The
+                        reason is to perform the tasks assigned to them on our
+                        behalf. However, they are obligated not to disclose or
+                        use the information for any other purpose.
+                    </p>
+                </div>
+
+                <h2>Security</h2>
+                <p className="paragraph">
                     We value your trust in providing us your Personal
                     Information, thus we are striving to use commercially
                     acceptable means of protecting it. But remember that no
@@ -89,8 +104,8 @@ export default function Policy() {
                     electronic storage is 100% secure and reliable, and we
                     cannot guarantee its absolute security.
                 </p>
-                <h3>Children’s Privacy</h3>
-                <p>
+                <h2>Children’s Privacy</h2>
+                <p className="paragraph">
                     This Services do not address anyone under the age of 13. We
                     do not knowingly collect personal identifiable information
                     from children under 13. In the case we discover that a child
@@ -100,22 +115,14 @@ export default function Policy() {
                     provided us with personal information, please contact us so
                     that we will be able to do necessary actions.
                 </p>
-                <h3>Changes to This Privacy Policy</h3>
-                <p>
+                <h2>Changes to This Privacy Policy</h2>
+                <p className="paragraph">
                     We may update our Privacy Policy from time to time. Thus,
                     you are advised to review this page periodically for any
                     changes. We will notify you of any changes by posting the
                     new Privacy Policy on this page. These changes are effective
                     immediately, after they are posted on this page.
                 </p>
-                <h3>Contact Us</h3>
-                <p>
-                    If you have any questions or suggestions about our Privacy
-                    Policy, do not hesitate to contact us. Contact Information:
-                </p>
-                <ul>
-                    <li>Email: contact@abdellatif.dev</li>
-                </ul>
             </div>
         </main>
     );
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,195 @@
+export default function Terms() {
+    return (
+        <main>
+            <div className="flex flex-col gap-8">
+                <p>
+                    <strong>Effective Date:</strong> 22/09/2024
+                </p>
+                <h1 className="terms-of-service">Terms of Service</h1>
+                <div className="paragraph">
+                    <p>
+                        Welcome to <strong>Abdellatif Dev</strong>. By engaging
+                        our services, you agree to the following Terms of
+                        Service, which outline the conditions for using our IT
+                        consultancy, software engineering, and software
+                        development services. Please read these terms carefully
+                        before proceeding.
+                    </p>
+                </div>
+                <h2 className="overview">
+                    1. <strong>Overview</strong>
+                </h2>
+                <p className="paragraph">
+                    Abdellatif Dev provides IT consultancy, software
+                    engineering, and freelance software development services. By
+                    accessing or using our services, you agree to abide by these
+                    terms, as well as any policies referenced herein. These
+                    terms apply to all clients and users of our services.
+                </p>
+                <h2 className="services-provided">
+                    2. <strong>Services Provided</strong>
+                </h2>
+                <div className="paragraph">
+                    <p>We offer the following services:</p>
+                    <ul>
+                        <li className="item">IT consultancy</li>
+                        <li className="item">Software engineering solutions</li>
+                        <li className="item">Custom software development</li>
+                        <li className="item">Web and mobile app development</li>
+                        <li className="item">System architecture and design</li>
+                        <li className="item">
+                            Technical support and maintenance
+                        </li>
+                    </ul>
+                    <p>
+                        Specific service details, timelines, and deliverables
+                        will be outlined in individual contracts or project
+                        agreements.
+                    </p>
+                </div>
+                <h2 className="client-obligations">
+                    3. <strong>Client Obligations</strong>
+                </h2>
+                <div className="paragraph">
+                    <ul>
+                        <li className="item">
+                            <strong>Project Scope:</strong> Clients must provide
+                            clear, detailed requirements before the commencement
+                            of a project. Any changes to the scope of work must
+                            be communicated in writing and may require
+                            adjustments to timelines and fees.
+                        </li>
+                        <li className="item">
+                            <strong>Timely Communication:</strong> Clients are
+                            expected to respond promptly to requests for
+                            information, approvals, and feedback to avoid
+                            project delays.
+                        </li>
+                        <li className="item">
+                            <strong>Payment Obligations:</strong> Payment terms
+                            will be specified in the individual project
+                            contract. Late payments may incur additional fees.
+                            Non-payment may result in suspension or termination
+                            of services.
+                        </li>
+                    </ul>
+                </div>
+                <h2 className="fees-payment-terms">
+                    4. <strong>Fees &amp; Payment Terms</strong>
+                </h2>
+                <div className="paragraph">
+                    <ul>
+                        <li className="item">
+                            <strong>Hourly/Fixed Rates:</strong> Fees will be
+                            based on either hourly rates or fixed project costs,
+                            as outlined in the specific project agreement.
+                        </li>
+                        <li className="item">
+                            <strong>Invoicing:</strong> Invoices will be sent
+                            based on milestones or project completion, depending
+                            on the terms agreed upon. Payment is due within 14
+                            days of receipt of the invoice.
+                        </li>
+                        <li className="item">
+                            <strong>Late Payments:</strong> Failure to pay on
+                            time may result in late fees, service suspension, or
+                            termination.
+                        </li>
+                    </ul>
+                </div>
+                <h2 className="confidentiality">
+                    5. <strong>Confidentiality</strong>
+                </h2>
+                <p className="paragraph">
+                    We understand the importance of maintaining confidentiality
+                    for all projects. Both Abdellatif Dev and the client agree
+                    to protect confidential information shared during the course
+                    of the project, including but not limited to trade secrets,
+                    business information, and proprietary code. This obligation
+                    will continue after the project’s completion.
+                </p>
+                <h2 className="intellectual-property">
+                    6. <strong>Intellectual Property</strong>
+                </h2>
+                <div className="paragraph">
+                    <ul>
+                        <li className="item">
+                            <strong>Client Ownership:</strong> Upon full
+                            payment, all deliverables and intellectual property
+                            rights related to the completed project will be
+                            transferred to the client unless otherwise agreed in
+                            writing.
+                        </li>
+                        <li className="item">
+                            <strong>Developer Rights:</strong> Abdellatif Dev
+                            retains the right to showcase general project
+                            details (excluding confidential information) in
+                            portfolios or marketing materials.
+                        </li>
+                    </ul>
+                </div>
+                <h2 className="warranties-and-limitation-of-liability">
+                    7. <strong>Warranties and Limitation of Liability</strong>
+                </h2>
+                <div className="paragraph">
+                    <ul>
+                        <li className="item">
+                            <strong>No Guarantee:</strong> While we strive for
+                            excellence, we do not guarantee that the delivered
+                            software will be error-free or that it will function
+                            without interruption.
+                        </li>
+                        <li className="item">
+                            <strong>Limitation of Liability:</strong> In no
+                            event will Abdellatif Dev be liable for any
+                            indirect, incidental, special, or consequential
+                            damages, including but not limited to loss of data,
+                            profits, or revenue arising from the use of our
+                            services.
+                        </li>
+                    </ul>
+                </div>
+                <h2 className="project-delays-modifications">
+                    8. <strong>Project Delays &amp; Modifications</strong>
+                </h2>
+                <p className="paragraph">
+                    If a project is delayed due to the client’s failure to
+                    provide necessary information or feedback, the agreed-upon
+                    timeline may be adjusted. Any modifications to the project
+                    scope after the project has commenced may result in
+                    additional fees and extended deadlines.
+                </p>
+                <h2 className="termination-of-services">
+                    9. <strong>Termination of Services</strong>
+                </h2>
+                <p className="paragraph">
+                    Either party may terminate the agreement for services by
+                    providing written notice. If a project is terminated before
+                    completion, the client will be responsible for payment for
+                    any work completed up to the point of termination.
+                    Abdellatif Dev may also terminate services due to
+                    non-payment or breach of these terms by the client.
+                </p>
+                <h2 className="governing-law">
+                    10. <strong>Governing Law</strong>
+                </h2>
+                <p className="paragraph">
+                    These Terms of Service are governed by and construed in
+                    accordance with the laws of Morocco. Any disputes arising
+                    from these terms shall be resolved through mediation or in
+                    the courts of Morocco.
+                </p>
+                <h2 className="amendments">
+                    11. <strong>Amendments</strong>
+                </h2>
+                <p className="paragraph">
+                    Abdellatif Dev reserves the right to update or modify these
+                    Terms of Service at any time. Clients will be notified of
+                    significant changes, and continued use of our services after
+                    such changes will constitute acceptance of the revised
+                    terms.
+                </p>
+            </div>
+        </main>
+    );
+}
